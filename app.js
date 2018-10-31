f8418d8e046df27b1d673c3053d16c25761134cf
ccades <hellocaden@gmail.com>
2018-10-30 18:55:29 -0700

    got a list to add individual items and clear all

--- app.js
+++ app.js
@@ -7,24 +7,59 @@ update display with new text value
  */
 
 $(document).ready(function(){
-  console.log("before\n", window.localStorage);
 
-  // add event listener
-  $(".add-text-btn").on("click", function(){
-    $(".show-text").empty();
-    var curTextValue = $('#theKey').val(); // reading from <input>
-    var curKeyValue = "theKey"; // change to dynamic key?
-    localStorage.setItem(curKeyValue, curTextValue);
-    $(".show-text").append(curTextValue);
-  });
+  var list = document.querySelector('#list');
+  var addBtn = document.querySelector('.addBtn');
+  var clearBtn = document.querySelector('.clearBtn');
+  var item = document.querySelector('#item');
 
-  // remove item from app
+  addBtn.addEventListener('click',function(e){
+    if (item.value.length > 0){
+      e.preventDefault();
+      list.innerHTML += '<li>' + item.value + '</li>';
+      store();
+      item.value = "";
+    } else {
+      alert('Please enter text')
+    }
+  },false)
 
-  // listen for click event (del)
-  $(".clear-cache-btn").on("click", function(){
-    // clear local storage
-    localStorage.clear();
-    $(".show-text").empty();
-  });
+  clearBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    list.innerHTML = ''
+    clearAll();
+  },false)
 
+  list.addEventListener('click',function(e){
+    var t = e.target;
+    console.log(e)
+    if(t.classList.contains('checked')){
+      t.parentNode.removeChild(t);
+    } else {
+      t.classList.add('checked');
+    }
+    store();
+  },false)
+
+  function store() {
+    window.localStorage.myitems = list.innerHTML;
+  }
+
+  function clearAll() {
+    window.localStorage.clear();
+  }
+
+  function getValues() {
+    var storedValues = window.localStorage.myitems;
+    if(!storedValues) {
+      list.innerHTML =
+      '<li>Step 1: Learn Javascript</li>'+
+      '<li>Step 2: ?</li>'+
+      '<li>Step 3: Profit!</li>';
+    }
+    else {
+      list.innerHTML = storedValues;
+    }
+  }
+  getValues();
 });
